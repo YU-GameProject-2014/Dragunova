40445afdc8d2251f04afdf1c421ba33542499cc9
U-hiroshi-PC\hiroshi <hiroshi@hiroshi-PC.(none)>
2016-05-23 13:29:36 +0900

    make simple tetris

---
+++ Main/Assets/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	//Groups
+	public GameObject[] groups;
+	
+	public void spawnNext(){
+		//Random Index
+		int i=Random.Range(0,groups.Length);
+		
+		//spawn Group at current Position
+		Instantiate(groups[i],
+		            transform.position,
+		            Quaternion.identity);
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+		spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
